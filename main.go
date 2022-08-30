1461df119ec4940f80eb32ee4f78214caeed6f60
Ade Fitriana <adefitriana007@gmail.com>
2022-08-30 16:49:58 +0700

    firstBE

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	// On Terminal/Command Propt
+  fmt.Println("Hello World!")
+
+	// On http (API)
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", func (w http.ResponseWriter, r *http.Request){
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello World"))
+	}).Methods("GET")
+
+	fmt.Println("server running localhost:5000")
+	http.ListenAndServe("localhost:5000", r)
+}
