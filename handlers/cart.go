c18443eebc6e6504c3d08a2e356fcc03676d1662
Ade Fitriana <adefitriana007@gmail.com>
2022-09-05 07:40:53 +0700

    update image

--- handlers/cart.go
+++ handlers/cart.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"os"
 	"strconv"
 	cartdto "waysbeans/dto/cart.go"
 	dto "waysbeans/dto/result"
@@ -209,9 +208,9 @@ func (h *handlersCart) FindCartsByID(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(response)
 	}
 
-	for i, p := range cart {
-		cart[i].Product.Image = os.Getenv("PATH_FILE") + p.Product.Image
-	}
+	// for i, p := range cart {
+	// 	cart[i].Product.Image = os.Getenv("PATH_FILE") + p.Product.Image
+	// }
 
 	w.WriteHeader(http.StatusOK)
 	response := dto.SuccessResult{Code: http.StatusOK, Data: cart}
